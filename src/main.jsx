0be9d5152370d34148fca58f96be39661f599718
Brianpham0204 <brianpham0204@gmail.com>
2026-09-02 10:24:17 +0200

    Clear legacy service worker cache

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './App.css';
 
+function clearLegacyAppCache() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations()
+      .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+      .catch(() => {});
+  }
+
+  if ('caches' in window) {
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .catch(() => {});
+  }
+}
+
+clearLegacyAppCache();
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
